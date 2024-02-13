12f1aba41d115e511217a76e56edab4e84cf6836
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-13 11:33:12 -0300

    agregamos el componente spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,53 @@
-import { Button, Image } from "react-bootstrap";
+import { Button, Container, Image, Spinner } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Frase from "./components/Frase";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
-  const[personaje,setPersonaje] = useState({});
+  useEffect(() => {
+    consultarAPI();
+  }, []);
 
-  useEffect(()=>{
-   consultarAPI();
-  },[])
+  const consultarAPI = async () => {
+    //hacer una peticion get a la api
+    const respuesta = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
+    const datos = await respuesta.json();
+    console.log(datos[0]);
+    setPersonaje(datos[0]);
+    //ocultar el spinner
+    setMostrarSpinner (false)
+  };
 
- const consultarAPI = async() =>{
-  //hacer una peticion get a la api
-  const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
-  const datos = await respuesta.json();
-  console.log(datos[0]);
-  setPersonaje(datos[0])
-
- }
+  const mostrarComponente = mostrarSpinner ? (
+    <div className="my-4">
+      <Spinner animation="border" variant="light"></Spinner>
+    </div>
+  ) : (
+    <Frase personaje={personaje}></Frase>
+  );
 
   return (
     <>
-      <section>
-        <div className="w-100 d-flex justify-content-center">
-          <Image
-            className="losSimpson mt-2"
-            src="img/The_Simpsons_yellow_logo.svg.png"
-            alt="Logo los Simpson"
-            fluid
-          />
-        </div>
-        <Frase personaje={personaje}></Frase>
-      </section>
+      <Container className="text-center my-5">
+        <Image
+          className="losSimpson mt-2"
+          src="img/The_Simpsons_yellow_logo.svg.png"
+          alt="Logo los Simpson"
+          fluid
+        />
+        {mostrarComponente}
+      </Container>
       <section className="d-flex justify-content-center mb-5">
-        <Button onClick={consultarAPI} className="bg-warning border border-warning text-dark fs-5 ">
+        <Button
+          onClick={consultarAPI}
+          className="bg-warning border border-warning text-dark fs-5 "
+        >
           Obtener frase
         </Button>
       </section>
